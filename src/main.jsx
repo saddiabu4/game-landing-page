70cc1e2c786c93c3092138eae95501d431d329f5
Abdulatif <saddiabu4@gmail.com>
2025-11-27 22:07:12 +0500

    Initialize React project with Vite, Tailwind CSS, and ESLint setup
    - Created package.json with dependencies for React, Tailwind CSS, and development tools.
    - Added Vite configuration for React and Tailwind CSS integration.
    - Included main entry point (main.jsx) with a simple React component.
    - Added global styles using Tailwind CSS in index.css.
    - Added Vite logo SVG to the public directory.

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<h1 className='text-3xl font-bold text-amber-600'>salom</h1>
+	</StrictMode>
+)
